d6aec91425b1b2f470aaec15d331b29455c69a70
rya23 <aaryathakur48@gmail.com>
2024-04-29 16:24:26 +0530

    Added Files

---
+++ kmp.cpp
@@ -0,0 +1,30 @@
+#include <bits\stdc++.h>
+
+using namespace std;
+
+
+
+
+
+void KMPSearch(string pattern,string text)
+{
+
+}
+
+
+
+
+void computerlsp(string pattern,string text,)
+
+
+
+
+
+
+int main()
+{
+    string text = "ABABDABACDABABCABAB";
+    string pattern = "ABABCABAB";
+    KMPSearch(pattern,text);
+    return 0;
+}
